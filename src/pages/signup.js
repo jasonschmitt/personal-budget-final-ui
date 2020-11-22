df284a31b6c70c6ad2684fdca8a4145fcc2ee22f
jschmitt <jasonschmittcode@gmail.com>
2020-11-22 14:49:21 -0500

    setting up initial sign up form

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -2,7 +2,33 @@ import React from 'react'
 
 class Signup extends React.Component {
   render() {
-    return <div>Signup page</div>
+    return (
+      <div className="row">
+        <form className="col s12">
+          <div className="row">
+            <div className="input-field col s6">
+              <input
+                placeholder="Placeholder"
+                id="first_name"
+                type="text"
+                className="validate"
+              />
+              <label htmlFor="first_name">First Name</label>
+            </div>
+            <div className="input-field col s6">
+              <input id="last_name" type="text" className="validate" />
+              <label htmlFor="last_name">Last Name</label>
+            </div>
+          </div>
+          <div className="row">
+            <div className="input-field col s12">
+              <input id="password" type="password" className="validate" />
+              <label htmlFor="password">Password</label>
+            </div>
+          </div>
+        </form>
+      </div>
+    )
   }
 }
 
